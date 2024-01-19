85a10c8d75c54c906d66572fe2728d6c82728a3b
ADXID12 <131492273+ADXID12@users.noreply.github.com>
2024-01-19 13:32:31 -0600

    Add files via upload

---
+++ Lab1P2_GabrielOsorto/Lab1P2_GabirelOsorto/src/lab1p2_gabirelosorto/Lab1P2_GabirelOsorto.java
@@ -0,0 +1,13 @@
+package lab1p2_gabirelosorto;
+
+import java.util.*;
+
+public class Lab1P2_GabirelOsorto {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        
+    }
+
+}
